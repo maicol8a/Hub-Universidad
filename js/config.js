4a5b287dd2f1dc12c3977975f81c561a05891725
Maicol Ochoa <maicol.ochoa@gmail.com>
2026-05-24 18:17:28 +0200

    Update Supabase key to a new secret

--- js/config.js
+++ js/config.js
@@ -1,6 +1,4 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
-const SUPABASE_URL = 'https://jivjhtqfqktmufmyeyzj.supabase.co'  // tu Project URL
-const SUPABASE_KEY = 'sb_publishable_FGMSjudy2IdTe2igi5JRJQ_Paw90L8J' // tu clave publishable
-
+const SUPABASE_URL = 'https://jivjhtqfqktmufmyeyzj.supabase.co'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImppdmpodHFmcWt0bXVmbXlleXpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQxMTU2NDYsImV4cCI6MjA4OTY5MTY0Nn0.xdOe0wzuQgydkVqyMf09PNuiWbeEzPCjLU9SeKpfR6M'
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
